70c3fd37f4b85a20d3c469f02fe6087b2c64a8d4
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2022-11-04 15:09:32 +0300

    last 2

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,70 @@
-import "./profile.scss"
+import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
   return (
-    <div className='profile'>Profile</div>
-  )
-}
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/13918756/pexels-photo-13918756.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="cover"></img>
+        <img
+          src="https://images.pexels.com/photos/13918756/pexels-photo-13918756.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="profilePic"></img>
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span> Umut Şahinkaya </span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>TUR</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>LeBook.dev</span>
+              </div>
+              <button>Follow</button>
+            </div>
+          </div>
+          <div className="right">
+          <EmailOutlinedIcon />
+          <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
